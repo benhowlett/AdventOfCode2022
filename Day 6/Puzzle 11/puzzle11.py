63cc28f23d1ad182aa0e87ef2bbf5663ef71f760
Ben Howlett <ben@benhowlett.com>
2022-12-09 07:13:57 -0700

    Day 6 puzzle 1 solution

---
+++ Day 6/Puzzle 11/puzzle11.py
@@ -0,0 +1,22 @@
+def allItemsAreUnique(list):
+    areUnique = True
+    for item in list:
+        if list.count(item) > 1:
+            areUnique = False
+    return areUnique
+
+dataStream = open('input.txt', 'r').readline()
+
+firstMarkerPosition = 0
+rollingBuffer = []
+
+for item in dataStream:
+    if len(rollingBuffer) == 4:
+        if allItemsAreUnique(rollingBuffer):
+            break
+        del rollingBuffer[0]
+    rollingBuffer.append(item) 
+    firstMarkerPosition += 1   
+    
+
+print(firstMarkerPosition)
