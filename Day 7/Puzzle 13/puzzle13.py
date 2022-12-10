40125afc344576986b71ea1ac01ccce556c5bd48
Ben Howlett <ben@benhowlett.com>
2022-12-10 09:17:37 -0700

    Day 7 puzzle 1 solution

---
+++ Day 7/Puzzle 13/puzzle13.py
@@ -0,0 +1,97 @@
+class Directory:
+    def __init__(self, name):
+        self.name = name
+        self.subDirectories = []
+        self.files = []
+        self.allDirectorySizes = []
+
+    def addSubDirectory(self, subDirectoryName):
+        self.subDirectories.append(Directory(subDirectoryName))
+
+    def addFile(self, file):
+        self.files.append(file)
+
+    def getFilesSize(self):
+        sumOfFileSizes = 0
+        for file in self.files:
+            sumOfFileSizes += file.size
+        return sumOfFileSizes
+    
+    def getTotalDirectorySize(self):
+        directorySize = self.getFilesSize()
+        for subDirectory in self.subDirectories:
+            directorySize += subDirectory.getTotalDirectorySize()
+        return directorySize
+
+    def getSubdirectoryByName(self, subDirectoryName):
+        for subDirectory in self.subDirectories:
+            if subDirectory.name == subDirectoryName:
+                return subDirectory
+
+    def traverseDirectoryAndGetSizes(self):
+        self.allDirectorySizes.append(self.getTotalDirectorySize())
+        for subDirectory in self.subDirectories:
+            subDirectory.traverseDirectoryAndGetSizes()
+            for size in subDirectory.allDirectorySizes:
+                self.allDirectorySizes.append(size)
+
+  
+
+class File:
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+
+
+root = Directory("/")
+currentDirectory = root
+path = []
+directorySizes = []
+currentlyListing = False
+
+for line in open('input.txt', 'r'):
+
+    if line.split()[0] == "$":
+
+        if line.split()[1] == "cd":
+
+            if line.split()[2] == "..":
+                path.pop()
+                tempDirectory = root
+                for step in path:
+                    tempDirectory = tempDirectory.getSubdirectoryByName(step)
+                currentDirectory = tempDirectory
+            
+            elif line.split()[2] == "/":
+                currentDirectory = root
+
+            else:
+                currentDirectory = currentDirectory.getSubdirectoryByName(line.split()[2])
+                path.append(line.split()[2])
+    else:
+
+        if line.split()[0] == "dir":
+
+            currentDirectory.addSubDirectory(line.split()[1])
+
+        else:
+
+            currentDirectory.addFile(File(line.split()[1], int(line.split()[0])))
+
+root.traverseDirectoryAndGetSizes()
+
+sumOfTotalsUnderThreshold = 0
+
+for size in root.allDirectorySizes:
+    if size <= 100000:
+        sumOfTotalsUnderThreshold += size
+
+print(sumOfTotalsUnderThreshold)
+
+
+
+
+        
+
+
+                
